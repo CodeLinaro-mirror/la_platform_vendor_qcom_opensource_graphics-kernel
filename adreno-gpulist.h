2202ec9317a9186b4a160595a245947c0c763de1
Urvashi Agrawal <quic_urvaagra@quicinc.com>
2025-04-14 14:55:16 -0700

    kgsl: gen8: Enable minBW feature for gen8_2_0
    Enable minBW for gen8_2_0.
    
    Change-Id: I567ad4eec9295ab8bdcea43044732c8b6ed016e0
    Signed-off-by: Urvashi Agrawal <quic_urvaagra@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -3278,7 +3278,8 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_2_0 = {
 			ADRENO_PREEMPTION | ADRENO_LPAC | ADRENO_AQE |
 			ADRENO_GMU_WARMBOOT | ADRENO_IFPC | ADRENO_CONTENT_PROTECTION |
 			ADRENO_HW_FENCE | ADRENO_BCL | ADRENO_ACD | ADRENO_GMU_BASED_DCVS |
-			ADRENO_GMU_THERMAL_MITIGATION | ADRENO_CLX | ADRENO_DEFER_GMEM_ALLOC,
+			ADRENO_GMU_THERMAL_MITIGATION | ADRENO_CLX | ADRENO_DEFER_GMEM_ALLOC |
+			ADRENO_GMU_MINBW,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_perfcounters,
 		.uche_gmem_alignment = SZ_64M,

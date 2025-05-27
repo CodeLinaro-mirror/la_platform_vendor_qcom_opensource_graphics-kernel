fabdd5967a1f79ac01f9ec811b939d123d133db4
Himanshu Agrawal <quic_c_hagraw@quicinc.com>
2025-05-27 15:36:11 +0530

    kgsl: build: Temporarily eliminate QMAA dependency.
    Change-Id: I36c17b36e0637bd9c79d7dfdc841659c1a573663
    Signed-off-by: Himanshu Agrawal <quic_c_hagraw@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -1,11 +1,13 @@
-ifeq ($(TARGET_USES_QMAA),true)
-        KGSL_ENABLED := false
-        ifeq ($(TARGET_USES_QMAA_OVERRIDE_GFX),true)
-                KGSL_ENABLED := true
-        endif # TARGET_USES_QMAA_OVERRIDE_GFX
-else
-        KGSL_ENABLED := true
-endif # TARGET_USES_QMAA
+#ifeq ($(TARGET_USES_QMAA),true)
+#        KGSL_ENABLED := false
+#        ifeq ($(TARGET_USES_QMAA_OVERRIDE_GFX),true)
+#                KGSL_ENABLED := true
+#        endif # TARGET_USES_QMAA_OVERRIDE_GFX
+#else
+#        KGSL_ENABLED := true
+#endif # TARGET_USES_QMAA
+
+KGSL_ENABLED := true
 
 ifeq ($(ENABLE_HYP), true)
         KGSL_ENABLED := false

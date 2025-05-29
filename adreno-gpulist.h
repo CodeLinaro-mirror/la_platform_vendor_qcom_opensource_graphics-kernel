b7bf4417ddf87f408d2f92cd707a4b93936ab4b6
Mohammed Mirza Mandayappurath Manzoor <mmandaya@qti.qualcomm.com>
2025-05-29 12:27:13 -0700

    kgsl: gen8: Setup GEN8_SP_CHICKEN_BITS_4 on gen8_2_x
    Setup the GEN8_SP_CHICKEN_BITS_4 non-context register on gen8_2_x GPUs.
    
    Change-Id: Id28b16e67cb5634fd46ea327b2d70eb5889680de
    Signed-off-by: Carter Cooper <ccooper@qti.qualcomm.com>
    Signed-off-by: Mohammed Mirza Mandayappurath Manzoor <mmandaya@qti.qualcomm.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2002,2007-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #define ANY_ID (~0)
@@ -3229,6 +3229,8 @@ static const struct gen8_nonctxt_regs gen8_2_0_nonctxt_regs[] = {
 	/* Disable PS out of order retire */
 	{ GEN8_SP_CHICKEN_BITS_2, 0xc21800, BIT(PIPE_NONE) },
 	{ GEN8_SP_CHICKEN_BITS_3, 0x00300000, BIT(PIPE_NONE) },
+	/* Disable SP2TP info attribute */
+	{ GEN8_SP_CHICKEN_BITS_4, 0x00000002, BIT(PIPE_NONE) },
 	{ GEN8_SP_PERFCTR_SHADER_MASK, 0x0000003f, BIT(PIPE_NONE) },
 	/* Ignore HLSQ shared constant feedback from SP */
 	{ GEN8_SP_HLSQ_DBG_ECO_CNTL_1, BIT(17), BIT(PIPE_NONE) },

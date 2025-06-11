1b2376d1d3e9ef4d5174038a1f6fc0287b686f1c
Carter Cooper <ccooper@qti.qualcomm.com>
2025-06-11 12:43:01 -0600

    kgsl: gen8: Disable the DINT data forwarding feature in CCU for gen8_2_x
    Disable the DINT data forwarding feature in CCU.
    
    Change-Id: I94878650662e694c7cdd0ce3402ea6c630a31785
    Signed-off-by: Carter Cooper <ccooper@qti.qualcomm.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -3314,8 +3314,8 @@ static const struct gen8_nonctxt_regs gen8_2_0_nonctxt_regs[] = {
 	/* Disable Dead Draw Merge scheme on RB-HLSQ */
 	{ GEN8_RB_RBP_CNTL, BIT(5), BIT(PIPE_BV) | BIT(PIPE_BR) },
 	{ GEN8_RB_CCU_CNTL, 0x00000068, BIT(PIPE_BR) },
-	/* Partially enable perf clear */
-	{ GEN8_RB_CCU_DBG_ECO_CNTL, 0x00002000, BIT(PIPE_BR) },
+	/* Partially enable perf clear, Disable DINT to c/z be data forwarding */
+	{ GEN8_RB_CCU_DBG_ECO_CNTL, 0x00002200, BIT(PIPE_BR) },
 	{ GEN8_RB_GC_GMEM_PROTECT, 0x12000000, BIT(PIPE_BR) },
 	{ GEN8_RB_RESOLVE_PREFETCH_CNTL, 0x00000007, BIT(PIPE_BR) },
 	{ GEN8_RB_CMP_DBG_ECO_CNTL, 0x00004000, BIT(PIPE_BR) },

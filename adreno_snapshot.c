08afebcdecfe1e4c1f5de993efa8b1d07a83ea03
Phani Muralidhar Sistla <psistla@qti.qualcomm.com>
2025-07-01 17:20:26 +0530

    kgsl: Add NULL check for AQE firmware memory in SWSCHED path
    AQE firmware memory is allocated in the hwsched path. In
    swsched path, the address is NULL, so add a check to avoid
    NULL pointer dereference.
    
    Change-Id: I4a1238ef75dc0d9f17f49f8ee348db6308a4af29
    Signed-off-by: Phani Muralidhar Sistla <psistla@qti.qualcomm.com>

--- adreno_snapshot.c
+++ adreno_snapshot.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2012-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <linux/utsname.h>
@@ -1112,7 +1112,12 @@ static size_t adreno_snapshot_aqe(struct kgsl_device *device, u8 *buf,
 	u32 *data = (u32 *)(buf + sizeof(*header));
 	struct adreno_firmware *fw = ADRENO_FW(adreno_dev, ADRENO_FW_AQE);
 
-	if (!ADRENO_FEATURE(adreno_dev, ADRENO_AQE))
+	/*
+	 * AQE firmware memory is allocated in the hwsched path.
+	 * In swsched path, the address is NULL, so add a check
+	 * to avoid NULL pointer dereference.
+	 */
+	if ((!ADRENO_FEATURE(adreno_dev, ADRENO_AQE)) || (fw->memdesc == NULL))
 		return 0;
 
 	if (remain < DEBUG_SECTION_SZ(AQE_FW_SNAPSHOT_DWORDS)) {

a623214562cc6060009fd44ae2da24aff3bb4cb3
Nisarg Sheth <nsheth@qti.qualcomm.com>
2025-07-08 12:13:53 +0530

    kgsl: Make kgsl_memdesc state atomic
    Two racing threads can try to simultaneously update the state of
    a kgsl_memdesc causing missed update by one of the threads depending
    on the race order. To avoid this, make kgsl_memdesc state atomic.
    
    Change-Id: Id4db7ebc04040725b5e1989e4061114aea2dd0eb
    Signed-off-by: Nisarg Sheth <nsheth@qti.qualcomm.com>

--- adreno_gen8_gmu.c
+++ adreno_gen8_gmu.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023-2024, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <dt-bindings/power/qcom-rpmpd.h>
@@ -2144,7 +2144,7 @@ static void gen8_free_gmu_globals(struct gen8_gmu_device *gmu)
 
 		iommu_unmap(device->gmu_core.domain, md->gmuaddr, md->size);
 
-		if (md->priv & KGSL_MEMDESC_SYSMEM)
+		if (TEST_FLAG(KGSL_MEMDESC_SYSMEM, &md->priv))
 			kgsl_sharedmem_free(md);
 
 		memset(md, 0, sizeof(*md));

cb2be09261eaf60ee61257da589ee573823fb9ea
Shudan Liu <c_shudan@qti.qualcomm.com>
2025-07-10 17:28:41 +0800

    Revert "msm: kgsl: Give hint to SMMU for skipping TLB ops during slumber"
    This reverts commit 5816f6e6c8b85d8596d305f0090b87380cb39c58.
    
    Change-Id: I4c5e698820db38f5d105898f93b351ff71df6dff
    Signed-off-by: Shudan Liu <c_shudan@qti.qualcomm.com>
    Signed-off-by: Abhilash Kumar Subhash <abhilshk@qti.qualcomm.com>

--- adreno_a6xx_gmu.c
+++ adreno_a6xx_gmu.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 
 #include <dt-bindings/regulator/qcom,rpmh-regulator-levels.h>
@@ -511,7 +511,6 @@ int a6xx_gmu_enable_gdsc(struct adreno_device *adreno_dev)
 		dev_err(&gmu->pdev->dev,
 			"Failed to enable GMU CX gdsc, error %d\n", ret);
 
-	kgsl_mmu_send_tlb_hint(&device->mmu, false);
 	clear_bit(GMU_PRIV_CX_GDSC_WAIT, &gmu->flags);
 	return ret;
 }
@@ -519,9 +518,7 @@ int a6xx_gmu_enable_gdsc(struct adreno_device *adreno_dev)
 void a6xx_gmu_disable_gdsc(struct adreno_device *adreno_dev)
 {
 	struct a6xx_gmu_device *gmu = to_a6xx_gmu(adreno_dev);
-	struct kgsl_device *device = KGSL_DEVICE(adreno_dev);
 
-	kgsl_mmu_send_tlb_hint(&device->mmu, true);
 	reinit_completion(&gmu->gdsc_gate);
 
 	if (ADRENO_QUIRK(adreno_dev, ADRENO_QUIRK_CX_GDSC))
@@ -2415,6 +2412,13 @@ static int a6xx_gmu_boot(struct adreno_device *adreno_dev)
 	if (ret)
 		goto gdsc_off;
 
+	/*
+	 * TLB operations are skipped during slumber. Incase CX doesn't
+	 * go down, it can result in incorrect translations due to stale
+	 * TLB entries. Flush TLB before boot up to ensure fresh start.
+	 */
+	kgsl_mmu_flush_tlb(&device->mmu);
+
 	ret = a6xx_rscc_wakeup_sequence(adreno_dev);
 	if (ret)
 		goto clks_gdsc_off;
@@ -2513,9 +2517,6 @@ static void a6xx_send_tlb_hint(struct kgsl_device *device, bool val)
 	if (!gmu->domain)
 		return;
 
-#if (KERNEL_VERSION(5, 15, 0) <= LINUX_VERSION_CODE)
-	qcom_skip_tlb_management(&gmu->pdev->dev, val);
-#endif
 	if (!val)
 		iommu_flush_iotlb_all(gmu->domain);
 }

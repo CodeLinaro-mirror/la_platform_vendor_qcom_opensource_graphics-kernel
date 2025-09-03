2a578c5bd7dc0c7e88e69b11d651e264dcaf15e0
Dmitry Baryshkov <dbaryshk@qti.qualcomm.com>
2025-09-03 03:17:05 +0300

    kgsl: fix undefined platform_bus_type error
    Include platform_device.h in order to fix the undeclared
    platform_bus_type compiler error.
    
    Change-Id: I1a42fc4e842220964d55761f69a9e656bbfe36a1
    Signed-off-by: Dmitry Baryshkov <dbaryshk@qti.qualcomm.com>

--- kgsl_gmu_core.h
+++ kgsl_gmu_core.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * Copyright (c) 2018-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023, 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
  */
 #ifndef __KGSL_GMU_CORE_H
 #define __KGSL_GMU_CORE_H
@@ -531,6 +531,7 @@ static struct iommu_domain *gmu_core_iommu_domain_alloc(struct device *dev)
 	return iommu_paging_domain_alloc(dev);
 }
 #else
+#include <linux/platform_device.h>
 static struct iommu_domain *gmu_core_iommu_domain_alloc(struct device *dev)
 {
 	return iommu_domain_alloc(&platform_bus_type);

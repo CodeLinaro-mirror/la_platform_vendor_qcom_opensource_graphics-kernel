b3e6e2057c0b20c6be422ca617750ffde81b0685
Mohammed Mirza Mandayappurath Manzoor Mopan <mmandaya@qti.qualcomm.com>
2025-11-17 15:46:57 -0800

    msm: kgsl: Enable GMU based DCVS on Art
    Art targets support GMU based DCVS. Enable the feature flag.
    
    Change-Id: Ie2e148a4225fe468f9c26a317b08608cc687d7c4
    Signed-off-by: Mohammed Mirza Mandayappurath Manzoor Mopan <mmandaya@qti.qualcomm.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -4259,7 +4259,7 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_11_0 = {
 				  UINT_MAX, UINT_MAX, UINT_MAX, ANY_ID),
 		.compatible = "qcom,adreno-gpu-gen8-11-0",
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT | ADRENO_LPAC | ADRENO_PREEMPTION |
-			ADRENO_GMU_WARMBOOT,
+			ADRENO_GMU_WARMBOOT | ADRENO_GMU_BASED_DCVS,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_2_x_perfcounters,
 		.uche_gmem_alignment = SZ_64M,

2824b1799ed182f2e7c715ff674fd0888f567805
Siva Krishna Reddy Yaramala <syaramal@qti.qualcomm.com>
2025-12-01 23:34:30 +0530

    kgsl: Add support for a619_malabar gpu
    Add an entry in adreno gpu list to support a619_malabar gpu.
    
    Change-Id: I22376375717b26f00354d04298a51262ff3d59d9
    Signed-off-by: Siva Krishna Reddy Yaramala <syaramal@qti.qualcomm.com>

--- adreno.h
+++ adreno.h
@@ -1367,6 +1367,12 @@ static inline int adreno_is_a619_holi(struct adreno_device *adreno_dev)
 		"qcom,adreno-gpu-a619-holi");
 }
 
+static inline int adreno_is_a619_malabar(struct adreno_device *adreno_dev)
+{
+	return of_device_is_compatible(adreno_dev->dev.pdev->dev.of_node,
+		"qcom,adreno-gpu-a619-malabar");
+}
+
 static inline int adreno_is_a620(struct adreno_device *adreno_dev)
 {
 	unsigned int rev = ADRENO_GPUREV(adreno_dev);

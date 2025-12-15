9a094c259fb121c5b3541d86c4d9304220eb1748
Carter Cooper <ccooper@qti.qualcomm.com>
2025-12-15 12:02:03 -0700

    kgsl: gen8: Enable ACD for gen8_11_0 GPU
    Enable ACD feature for gen8_11_0 target.
    
    Change-Id: Ib8c97f2f1de88b2e900b7a3bf623342b695a2299
    Signed-off-by: Carter Cooper <ccooper@qti.qualcomm.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -4381,7 +4381,8 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_11_0 = {
 			ADRENO_GMU_WARMBOOT | ADRENO_GMU_BASED_DCVS | ADRENO_CLX |
 			ADRENO_GMU_THERMAL_MITIGATION | ADRENO_AQE | ADRENO_CONTENT_PROTECTION |
 			ADRENO_BCL | ADRENO_DCVS_PROFILE | ADRENO_IFPC | ADRENO_GMU_MINBW |
-			ADRENO_DEFER_GMEM_ALLOC | ADRENO_GMU_FAST_CONTEXT_DESTROY,
+			ADRENO_DEFER_GMEM_ALLOC | ADRENO_GMU_FAST_CONTEXT_DESTROY |
+			ADRENO_ACD,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_2_x_perfcounters,
 		.uche_gmem_alignment = SZ_64M,

7f30811896f9ae6ab64cdd2ba201dc1d7d482161
Urvashi Agrawal <urvaagra@qti.qualcomm.com>
2026-02-03 15:38:25 -0800

    kgsl: gen8: Add RBBM status info to snapshot
    Modify the slice mask debug ID to use the right value and
    also add the RBBM status as a debug information which
    will help the users to distinguish snapshots based on
    RBBM status information.
    
    Change-Id: Ie80223d5ee5b140a10071688c7446c8c052c3cb0
    Signed-off-by: Urvashi Agrawal <urvaagra@qti.qualcomm.com>

--- adreno_gen8_snapshot.c
+++ adreno_gen8_snapshot.c
@@ -820,6 +820,25 @@ err_clk_put:
 	clk_put(clk);
 }
 
+static size_t gen8_snapshot_rbbm_status(struct kgsl_device *device, u8 *buf,
+		size_t remain, void *priv)
+{
+	struct kgsl_snapshot_debug *header = (struct kgsl_snapshot_debug *)buf;
+	u32 *data = (u32 *)(buf + sizeof(*header));
+
+	if (remain < DEBUG_SECTION_SZ(1)) {
+		SNAPSHOT_ERR_NOMEM(device, "RBBM STATUS");
+		return 0;
+	}
+
+	/* Dump the rbbm status information */
+	header->type = SNAPSHOT_DEBUG_RBBM_STATUS;
+	header->size = 1;
+	kgsl_regread(device, GEN8_RBBM_STATUS, data);
+
+	return DEBUG_SECTION_SZ(1);
+}
+
 static size_t gen8_snapshot_slice_mask(struct kgsl_device *device, u8 *buf,
 		size_t remain, void *priv)
 {
@@ -1959,6 +1978,9 @@ void gen8_snapshot(struct adreno_device *adreno_dev,
 
 	kgsl_snapshot_add_section(device, KGSL_SNAPSHOT_SECTION_DEBUG,
 		snapshot, gen8_snapshot_slice_mask, NULL);
+
+	kgsl_snapshot_add_section(device, KGSL_SNAPSHOT_SECTION_DEBUG,
+		snapshot, gen8_snapshot_rbbm_status, NULL);
 
 	gen8_snapshot_cx_debugbus(adreno_dev, snapshot);
 

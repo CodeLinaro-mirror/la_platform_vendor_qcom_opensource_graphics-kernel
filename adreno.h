23a491c39550b15ca669ede279b6170c421f5f90
Gaurav Singh <gauravsi@qti.qualcomm.com>
2026-02-09 04:48:55 -0800

    kgsl: gen8: Add gen8_14_0 GPU core
    Add a core to the GPU list for the gen8_14_0 target.
    
    Change-Id: I63cfb9875813393094813fca364537182edb459c
    Signed-off-by: Gaurav Singh <gauravsi@qti.qualcomm.com>

--- adreno.h
+++ adreno.h
@@ -320,6 +320,7 @@ enum adreno_gpurev {
 	ADRENO_REV_GEN8_9_0 = ADRENO_GPUREV_VALUE(8, 9, 0),
 	ADRENO_REV_GEN8_11_0 = ADRENO_GPUREV_VALUE(8, 11, 0),
 	ADRENO_REV_GEN8_11_1 = ADRENO_GPUREV_VALUE(8, 11, 1),
+	ADRENO_REV_GEN8_14_0 = ADRENO_GPUREV_VALUE(8, 14, 0),
 	ADRENO_REV_GEN8_17_0 = ADRENO_GPUREV_VALUE(8, 17, 0),
 };
 
@@ -1483,6 +1484,7 @@ ADRENO_TARGET(gen8_8_0, ADRENO_REV_GEN8_8_0)
 ADRENO_TARGET(gen8_9_0, ADRENO_REV_GEN8_9_0)
 ADRENO_TARGET(gen8_11_0, ADRENO_REV_GEN8_11_0)
 ADRENO_TARGET(gen8_11_1, ADRENO_REV_GEN8_11_1)
+ADRENO_TARGET(gen8_14_0, ADRENO_REV_GEN8_14_0)
 ADRENO_TARGET(gen8_17_0, ADRENO_REV_GEN8_17_0)
 
 static inline int adreno_is_gen7_9_x(struct adreno_device *adreno_dev)
@@ -1511,7 +1513,8 @@ static inline int adreno_is_gen7_2_x_family(struct adreno_device *adreno_dev)
 
 static inline int adreno_is_gen8_11_x(struct adreno_device *adreno_dev)
 {
-	return adreno_is_gen8_11_0(adreno_dev) || adreno_is_gen8_11_1(adreno_dev);
+	return adreno_is_gen8_11_0(adreno_dev) || adreno_is_gen8_11_1(adreno_dev) ||
+		adreno_is_gen8_14_0(adreno_dev);
 }
 
 static inline int adreno_is_gen8_2_x(struct adreno_device *adreno_dev)

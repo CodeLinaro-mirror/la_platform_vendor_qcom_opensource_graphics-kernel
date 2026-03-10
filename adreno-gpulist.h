cd9fdf3371b4b155c605e7e00abda1e33f76a512
David Werner <daviwern@qti.qualcomm.com>
2026-03-10 14:02:28 -0700

    kgsl: gen8: Enable GPU SPEL for gen8_11_x
    Enable the GPU System Power and Energy Limits (SPEL) feature for
    the gen8_11_x targets. This feature works in conjunction with the
    GMU firmware to provide power budget allocations to the GPU and
    monitor compliance.
    
    Change-Id: I98cd6171826bba9745c5c096278162803a0037fa
    Signed-off-by: David Werner <daviwern@qti.qualcomm.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -4437,7 +4437,7 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_11_0 = {
 			ADRENO_GMU_THERMAL_MITIGATION | ADRENO_AQE | ADRENO_CONTENT_PROTECTION |
 			ADRENO_BCL | ADRENO_DCVS_PROFILE | ADRENO_IFPC | ADRENO_GMU_MINBW |
 			ADRENO_DEFER_GMEM_ALLOC | ADRENO_GMU_FAST_CONTEXT_DESTROY |
-			ADRENO_ACD | ADRENO_GMU_AB | ADRENO_HW_FENCE |
+			ADRENO_ACD | ADRENO_GMU_AB | ADRENO_HW_FENCE | ADRENO_GMU_SPEL |
 			ADRENO_AHB_TIMEOUT_RECOVERY,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_2_x_perfcounters,
@@ -4566,7 +4566,7 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_11_1 = {
 			ADRENO_GMU_THERMAL_MITIGATION | ADRENO_AQE | ADRENO_CONTENT_PROTECTION |
 			ADRENO_BCL | ADRENO_DCVS_PROFILE | ADRENO_IFPC | ADRENO_GMU_MINBW |
 			ADRENO_DEFER_GMEM_ALLOC | ADRENO_GMU_FAST_CONTEXT_DESTROY |
-			ADRENO_ACD | ADRENO_GMU_AB | ADRENO_HW_FENCE,
+			ADRENO_ACD | ADRENO_GMU_AB | ADRENO_HW_FENCE | ADRENO_GMU_SPEL,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_2_x_perfcounters,
 		.uche_gmem_alignment = SZ_64M,
